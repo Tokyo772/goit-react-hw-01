aa87899ae8b9c3df9b34445d525efd2e81c8170a
Oleksandr Humeniuk <merokyry33@gmail.com>
2024-05-06 18:08:39 +0200

    task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,12 @@ import "./App.css";
 
 import Profile from "./Profile";
 import userData from "./userData.json";
+
 import FriendList from "./FriendList";
 import friends from "./friends.json";
+
+import TransactionHistory from "./TransactionHistory";
+import transactions from "./transactions.json";
 
 const App = () => {
   return (
@@ -11,6 +15,7 @@ const App = () => {
       <div className="container">
         <Profile {...userData}></Profile>
         <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
       </div>
     </>
   );
---
+++ src/components/TransactionHistory.jsx
@@ -0,0 +1,26 @@
+import styles from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr className={styles.tr}>
+          <th className={styles.th}>Type</th>
+          <th className={styles.th}>Amount</th>
+          <th className={styles.th}>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ currency, amount, type, id }) => (
+          <tr key={id} className={styles.tr}>
+            <td className={styles.td}>{type}</td>
+            <td className={styles.td}>{amount}</td>
+            <td className={styles.td}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
